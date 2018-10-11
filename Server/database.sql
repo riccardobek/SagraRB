3007ad0623e438548cdd310f6f7936aa6680339b
riccardobek <37105282+riccardobek@users.noreply.github.com>
2018-10-11 18:10:22 +0200

    Generalized database.sql

--- Server/database.sql
+++ Server/database.sql
@@ -1,18 +1,32 @@
-DROP DATABASE IF EXISTS sagra;
-CREATE DATABASE sagra;
+DROP DATABASE IF EXISTS festival;
+CREATE DATABASE festival;
 
-USE sagra;
+USE festival;
 
-DROP TABLE IF EXISTS utenti;
-CREATE TABLE utenti(
+DROP TABLE IF EXISTS places;
+CREATE TABLE places(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
+  name VARCHAR(50) NOT NULL UNIQUE
+);
+
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
   id INTEGER AUTO_INCREMENT PRIMARY KEY ,
   username VARCHAR(50) NOT NULL UNIQUE ,
   password VARCHAR(256) NOT NULL,
-  tipo ENUM('user','admin') NOT NULL
+  type ENUM('user','admin') NOT NULL
 );
 
-INSERT INTO utenti(username, password, tipo) VALUES ('admin',SHA2('admin',512),'admin'),
+INSERT INTO users(username, password, type) VALUES ('admin',SHA2('admin',512),'admin'),
   ('user',SHA2('user',512),'user');
+
+DROP TABLE IF EXISTS UsersForPlaces;
+CREATE TABLE UsersForPlaces(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
+  idUser INTEGER NOT NULL REFERENCES  users(id),
+  idPlaces INTEGER NOT NULL REFERENCES places(id)
+);
 
 DROP TABLE IF EXISTS menu;
 CREATE TABLE menu(
@@ -21,3 +35,27 @@ CREATE TABLE menu(
   prezzo DOUBLE NOT NULL,
   tipo ENUM('Primo','Secondo','Contorno','Bibita','Dolce') NOT NULL
 );
+
+DROP TABLE IF EXISTS menuForPlaces;
+CREATE TABLE menuForPlaces(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
+  idMenu INTEGER NOT NULL REFERENCES menu(id),
+  idPlaces INTEGER NOT NULL REFERENCES places(id)
+);
+
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
+  name VARCHAR(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
+  idCostumer INTEGER NOT NULL REFERENCES customers(id),
+  quantity INTEGER NOT NULL,
+  idMenu INTEGER NOT NULL REFERENCES menu(id)
+);
+
+
+
