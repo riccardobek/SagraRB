86736ca663c30ca18ea99b365e790282a31ec91e
riccardobek <37105282+riccardobek@users.noreply.github.com>
2018-10-17 00:07:56 +0200

    Translated schema into SQL (to be checked)

--- Database/database.sql
+++ Database/database.sql
@@ -1,60 +1,69 @@
-DROP DATABASE IF EXISTS festival;
-CREATE DATABASE festival;
+DROP DATABASE IF EXISTS sagra;
+CREATE DATABASE sagra;
 
-USE festival;
+USE sagra;
 
-DROP TABLE IF EXISTS places;
-CREATE TABLE places(
-  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
-  name VARCHAR(50) NOT NULL UNIQUE
+DROP TABLE IF EXISTS luoghi;
+CREATE TABLE luoghi(
+  id_l INTEGER AUTO_INCREMENT PRIMARY KEY ,
+  name VARCHAR(50) NOT NULL UNIQUE,
+  numeroTavoli INTEGER
 );
 
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users(
-  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
+DROP TABLE IF EXISTS utenti;
+CREATE TABLE utenti(
+  id_u INTEGER AUTO_INCREMENT PRIMARY KEY ,
   username VARCHAR(50) NOT NULL UNIQUE ,
   password VARCHAR(256) NOT NULL,
-  type ENUM('user','admin') NOT NULL
+  tipo ENUM('user','admin') NOT NULL,
+  luogoAsseganto INTEGER,
+  FOREIGN KEY(luogoAsseganto) REFERENCES luoghi(id_l)
 );
 
 INSERT INTO users(username, password, type) VALUES ('admin',SHA2('admin',512),'admin'),
   ('user',SHA2('user',512),'user');
 
-DROP TABLE IF EXISTS UsersForPlaces;
-CREATE TABLE UsersForPlaces(
-  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
-  idUser INTEGER NOT NULL REFERENCES  users(id),
-  idPlaces INTEGER NOT NULL REFERENCES places(id)
-);
 
 DROP TABLE IF EXISTS menu;
 CREATE TABLE menu(
-  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
+  id_m INTEGER AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(50) NOT NULL UNIQUE ,
   prezzo DOUBLE NOT NULL,
   tipo ENUM('Primo','Secondo','Contorno','Bibita','Dolce') NOT NULL
 );
 
-DROP TABLE IF EXISTS menuForPlaces;
-CREATE TABLE menuForPlaces(
-  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
-  idMenu INTEGER NOT NULL REFERENCES menu(id),
-  idPlaces INTEGER NOT NULL REFERENCES places(id)
+DROP TABLE IF EXISTS menuServito;
+CREATE TABLE menuServito(
+  id_ms INTEGER AUTO_INCREMENT PRIMARY KEY ,
+  luogo INTEGER NOT NULL,
+  elementoMenu INTEGER NOT NULL,
+  FOREIGN KEY(luogo) REFERENCES luoghi(id_l),
+  FOREIGN KEY(elementoMenu) REFERENCES menu(id_m)
 );
 
-DROP TABLE IF EXISTS customers;
-CREATE TABLE customers(
-  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
-  name VARCHAR(50) NOT NULL
+DROP TABLE IF EXISTS ordini;
+CREATE TABLE ordini(
+  id_o INTEGER AUTO_INCREMENT PRIMARY KEY ,
+  nome INTEGER NOT NULL,
+  numeroCoperti INTEGER NOT NULL,
+  asporto BOOL NOT NULL,
+  ora TIME,
+  data DATE,
+  tavoloAsseganto INTEGER,
+  commesso INTEGER NOT NULL ,
+  FOREIGN KEY(commesso) REFERENCES utenti(id_u)
 );
 
-DROP TABLE IF EXISTS orders;
-CREATE TABLE orders(
+DROP TABLE IF EXISTS vendita;
+CREATE TABLE vendita(
   id INTEGER AUTO_INCREMENT PRIMARY KEY ,
-  idCostumer INTEGER NOT NULL REFERENCES customers(id),
-  quantity INTEGER NOT NULL,
-  idMenu INTEGER NOT NULL REFERENCES menu(id)
+  quantita INTEGER NOT NULL ,
+  formaggio BOOL,
+  ordine INTEGER NOT NULL ,
+  elementoMenu INTEGER NOT NULL ,
+  FOREIGN KEY(ordine) REFERENCES menu(id_o),
+  FOREIGN KEY(elementoMenu) REFERENCES menu(id_m)
 );
 
 
